38f8d907d3b06fbc4aec21c4cb8afc4f39c2b65e
Lucas <lucas.tnagel@gmail.com>
2025-09-04 18:42:48 -0300

    Bpf upstream

--- compiler/rustc_target/src/callconv/bpf.rs
+++ compiler/rustc_target/src/callconv/bpf.rs
@@ -3,9 +3,7 @@ use crate::abi::call::{ArgAbi, FnAbi};
 
 fn classify_ret<Ty>(ret: &mut ArgAbi<'_, Ty>) {
     if ret.layout.is_aggregate() || ret.layout.size.bits() > 64 {
-        if ret.layout.size.bits() != 128 {
-            ret.make_indirect();
-        }
+        ret.make_indirect();
     } else {
         ret.extend_integer_width_to(64);
     }
@@ -13,9 +11,7 @@ fn classify_ret<Ty>(ret: &mut ArgAbi<'_, Ty>) {
 
 fn classify_arg<Ty>(arg: &mut ArgAbi<'_, Ty>) {
     if arg.layout.is_aggregate() || arg.layout.size.bits() > 64 {
-        if arg.layout.size.bits() != 128 {
-            arg.make_indirect();
-        }
+        arg.make_indirect();
     } else {
         arg.extend_integer_width_to(64);
     }
